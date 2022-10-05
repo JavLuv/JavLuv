ff0574d7baa0abcce7105f0a64106010f6bfcc55
JavLuv <JavLuv@proton.me>
2022-10-05 14:34:07 -0700

    Fix issue with age-based sorting

--- src/MovieInfo/CmdSearchActresses.cs
+++ src/MovieInfo/CmdSearchActresses.cs
@@ -30,14 +30,15 @@ namespace MovieInfo
         public int Compare(ActressData left, ActressData right)
         {
             if (left.DobYear == 0 && right.DobYear == 0)
-                return String.Compare(left.Name, right.Name);
+                return String.Compare(right.Name, left.Name);
             if (left.DobYear == 0)
                 return 1;
             if (right.DobYear == 0)
                 return -1;
-            if (left.DobDay == 0 || left.DobMonth == 0 || right.DobDay == 0 || right.DobMonth == 0)
-                return (left.DobDay == right.DobDay) ? 0 : (left.DobDay < right.DobDay) ? 1 : -1;
-            return DateTime.Compare(new DateTime(right.DobYear, right.DobMonth, right.DobDay), new DateTime(left.DobYear, left.DobMonth, left.DobDay));
+            return DateTime.Compare(
+                new DateTime(right.DobYear, Math.Max(right.DobMonth, 1), Math.Max(right.DobDay, 1)),
+                new DateTime(left.DobYear, Math.Max(left.DobMonth, 1), Math.Max(left.DobDay, 1))
+            );
         }
     }
 
@@ -51,8 +52,10 @@ namespace MovieInfo
                 return 1;
             if (right.DobYear == 0)
                 return -1;
-            var c = new ActressAgeYoungestComparer();
-            return c.Compare(right, left);
+            return DateTime.Compare(
+                new DateTime(left.DobYear, Math.Max(left.DobMonth, 1), Math.Max(left.DobDay, 1)),
+                new DateTime(right.DobYear, Math.Max(right.DobMonth, 1), Math.Max(right.DobDay, 1))
+            );
         }
     }
 
